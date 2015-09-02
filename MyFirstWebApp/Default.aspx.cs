a8cec1f8cbebeff2a93f4ba7c8622c95126806ed
Scott Struessel <scott.struessel@outlook.com>
2015-09-02 05:08:40 -0600

    MyFirstWebApp -First Commit

---
+++ MyFirstWebApp/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+            string fullName = firstName + " " + lastName;
+
+            string result = "Hello " + firstName + " " + lastName;
+            resultLabel.Text = result;
+        }
+    }
+}
